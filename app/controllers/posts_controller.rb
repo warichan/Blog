3132d3f006df37d5c3951c25820fb8a4da647ada
warichan <txaxmxmxm@gmail.com>
2016-04-16 16:24:49 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+
+  http_basic_authenticate_with name: "user", password: "secret", except: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to post_path
+  end
+
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :text)
+end
+
+end
---
+++ db/migrate/20160415095355_add_details_to_comments.rb
@@ -0,0 +1,5 @@
+class AddDetailsToComments < ActiveRecord::Migration
+  def change
+    add_column :comments, :commenter, :string
+  end
+end
